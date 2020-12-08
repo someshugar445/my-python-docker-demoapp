c2a526b4fb74be2d87e9c587d087a211892ef1a8
Somesh Ugar <someshugar@gmail.com>
2020-12-08 19:16:44 +0530

    updated init.sql and app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,29 @@
+from typing import List, Dict
 from flask import Flask
+import mysql.connector
+import json
+
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+
+
+def favorite_users() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'users'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_users')
+    results = [{username: password, email: phone} for (username, password, email, phone) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+@app.route('/')
+def index() -> str:
+    return json.dumps({'favorite_users': favorite_users()})
--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,9 @@ CREATE DATABASE users;
 use users;
 
 CREATE TABLE favorite_users (
-  username VARCHAR(20) NOT NULL PRIMARY,
-  password VARCHAR(10),
-  email VARCHAR(15) NOT NULL UNIQUE,
+  username VARCHAR(20) NOT NULL PRIMARY KEY,
+  password VARCHAR(10) NOT NULL,
+  email VARCHAR(25) NOT NULL UNIQUE,
   phone VARCHAR(10)
 );
 
