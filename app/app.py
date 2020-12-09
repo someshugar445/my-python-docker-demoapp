b505ebd2d20612dab13d03a59c5e2e69ef24a4fe
Somesh Ugar <someshugar@gmail.com>
2020-12-09 16:14:31 +0530

    aded login register and logout apis

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 from typing import List, Dict
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session 
+import re 
 import mysql.connector
 import json
 
-app = Flask(__name__)
 
 
-def favorite_users() -> List[Dict]:
+def connect_to_db():
     config = {
         'user': 'root',
         'password': 'root',
@@ -16,14 +16,72 @@ def favorite_users() -> List[Dict]:
     }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM favorite_users')
-    results = [{username: password, email: phone} for (username, password, email, phone) in cursor]
+    return cursor
+
+def close_connection(cursor,connection):
+
     cursor.close()
     connection.close()
+ 
+    print("Connection to db is closed")
 
-    return results
 
+  
+  
+app = Flask(__name__) 
+  
+  
+app.secret_key = 'sadhdks1234'
 
-@app.route('/')
-def index() -> str:
-    return json.dumps({'favorite_users': favorite_users()})
+@app.route('/') 
+@app.route('/login', methods =['GET', 'POST']) 
+def login(): 
+    msg = '' 
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form: 
+        username = request.form['username'] 
+        password = request.form['password'] 
+        cursor = connect_to_db()
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, )) 
+        account = cursor.fetchone() 
+        if account: 
+            session['loggedin'] = True
+            session['id'] = account['id'] 
+            session['username'] = account['username'] 
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg) 
+        else: 
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg) 
+  
+@app.route('/logout') 
+def logout(): 
+    session.pop('loggedin', None) 
+    session.pop('id', None) 
+    session.pop('username', None) 
+    return redirect(url_for('login')) 
+  
+@app.route('/register', methods =['GET', 'POST']) 
+def register(): 
+    msg = '' 
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'phone' in request.form: 
+        username = request.form['username'] 
+        password = request.form['password'] 
+        email = request.form['email'] 
+        phone = request.form['phone'] 
+        cursor = connect_to_db() 
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, )) 
+        account = cursor.fetchone() 
+        if account: 
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username): 
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email or not phone: 
+            msg = 'Please fill out the form !'
+        else: 
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email,phone )) 
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST': 
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg) 
